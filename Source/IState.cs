b6dd36f90085046e2fcbd8f01bd2e5f91704a8fa
Vladislav Panin <vpd.aspid@gmail.com>
2025-12-16 11:28:48 +0300

    Initial commit

---
+++ Source/IState.cs
@@ -0,0 +1,10 @@
+// ReSharper disable once CheckNamespace
+namespace Aspid.Core.HSM
+{
+    public interface IState
+    {
+        public void Enter();
+        
+        public void Exit();
+    }
+}
