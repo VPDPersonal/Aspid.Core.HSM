4e6a6d39cee0fc5aba07020a3614339fe7d69599
Vladislav Panin <vpd.aspid@gmail.com>
2025-12-19 13:40:23 +0300

    Fix bugs and add StateExtensions

--- Unity/Runtime/Controllers/IFixedUpdateController.cs
+++ Unity/Runtime/Controllers/IFixedUpdateController.cs
@@ -1,7 +1,7 @@
 // ReSharper disable once CheckNamespace
 namespace Aspid.Core.HSM
 {
-    public interface IFixedUpdateController
+    public interface IFixedUpdateController : IController
     {
         public void FixedUpdate(float deltaTime);
     }
