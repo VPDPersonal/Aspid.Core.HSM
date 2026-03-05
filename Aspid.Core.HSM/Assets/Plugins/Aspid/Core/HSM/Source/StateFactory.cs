ec36e444c3a80a5093ae339d6fe63e2168bfa15a
Vladislav Panin <vpd.aspid@gmail.com>
2026-03-05 13:46:37 +0300

    Add Initialize method in StateFactory

--- Aspid.Core.HSM/Assets/Plugins/Aspid/Core/HSM/Source/StateFactory.cs
+++ Aspid.Core.HSM/Assets/Plugins/Aspid/Core/HSM/Source/StateFactory.cs
@@ -6,12 +6,14 @@ namespace Aspid.Core.HSM
 {
     public abstract class StateFactory
     {
+        private readonly HashSet<Type> _initializedStates = new();
+        
         public IEnumerable<IState> CreateState<TState>(IReadOnlyList<IState> activeStates)
             where TState : IState
         {
             return CreateState(typeof(TState), activeStates, index: activeStates.Count - 1);
         }
-
+        
         private IEnumerable<IState> CreateState(Type type, IReadOnlyList<IState> activeStates, int index)
         {
             if (index >= 0 && type == activeStates[index].GetType())
@@ -21,20 +23,31 @@ namespace Aspid.Core.HSM
             }
             else
             {
-                var state = CreateState(type);
+                var state = CreateStateInternal(type);
                 
                 if (state is IChildState childState)
                 {
                     foreach (var parentState in CreateState(childState.ParentState, activeStates, --index))
                         yield return parentState;
                 }
+
+                if (_initializedStates.Add(type))
+                    InitializeState(state);
                 
                 yield return state;
             }
         }
 
-        protected abstract IState CreateState(Type type);
+        protected abstract IState CreateStateInternal(Type type);
 
-        public abstract void Release(IState state);
+        protected virtual void InitializeState(IState state) { }
+
+        public void Release(IState state)
+        {
+            _initializedStates.Remove(state.GetType());
+            ReleaseInternal(state);
+        }
+
+        protected virtual void ReleaseInternal(IState state) { }
     }
 }
