07d663cf070df7ff8380ac8f7b45fef4963f8951
Rodion Gayvoronskiy <r.gayvoronskiy@carx-tech.com>
2026-04-23 13:05:46 +0300

    test state

---
+++ Aspid.Core.HSM/Assets/_Scripts/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Aspid.Core.HSM;
+using UnityEngine;
+
+namespace _Scripts.Controllers
+{
+	public class TestController : IController, IUpdateController
+	{
+		private readonly string _stateName;
+
+		public TestController(string stateName)
+		{
+			_stateName = stateName;
+		}
+		
+		public void Update(float deltaTime)
+		{
+			Debug.Log(_stateName);
+		}
+	}
+}
---
+++ Aspid.Core.HSM/Assets/_Scripts/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Aspid.Core.HSM;
+using UnityEngine;
+
+namespace _Scripts
+{
+	public class StateFactory : StateFactory<RootState>
+	{
+		protected override IState CreateStateInternal(Type type)
+		{
+			if(type == typeof(RootState))
+			{
+				return new RootState();
+			}
+			else if (type == typeof(SinglePlayerState))
+			{
+				return new SinglePlayerState();
+			}
+			return null;
+		}
+	}
+	
+	public class Test : MonoStateMachine
+	{
+		private void Awake()
+		{
+			var stateFactory = new StateFactory();
+			Initialize(stateFactory);
+		}
+
+		protected override void OnUpdating()
+		{
+			base.OnUpdating();
+
+			if (Input.GetKeyDown(KeyCode.Z))
+			{
+				ChangeState<RootState>();
+			}
+			
+			if (Input.GetKeyDown(KeyCode.X))
+			{
+				ChangeState<RootState>();
+			}
+		}
+	}
+}
