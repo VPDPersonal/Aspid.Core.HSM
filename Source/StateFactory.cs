4854ffd7d8145e0c5bb21002b98fc0f1769302ad
Vladislav Panin <vpd.aspid@gmail.com>
2026-04-28 15:26:18 +0300

    Add StateTree hierarchy and install UniCli server package
    Introduce IStateHierarchy / StateTree builder as an alternative to
    [ParentState]-driven IChildState: StateFactory now accepts an optional
    hierarchy and walks it to build the state chain. Adds UniCli server
    package via UPM git URL for editor automation.

--- Source/StateFactory.cs
+++ Source/StateFactory.cs
@@ -8,6 +8,14 @@ namespace Aspid.Core.HSM
     {
         private readonly HashSet<Type> _initializedStates = new();
         private readonly List<IState> _chainBuffer = new(capacity: 4);
+        private readonly IStateHierarchy? _hierarchy;
+
+        protected StateFactory() { }
+
+        protected StateFactory(IStateHierarchy hierarchy)
+        {
+            _hierarchy = hierarchy;
+        }
 
         public IReadOnlyList<IState> CreateState<TState>(IReadOnlyList<IState> activeStates)
             where TState : IState
@@ -23,6 +31,15 @@ namespace Aspid.Core.HSM
             {
                 for (var i = 0; i <= index; i++)
                     _chainBuffer.Add(activeStates[i]);
+                return;
+            }
+
+            if (_hierarchy != null)
+            {
+                if (_hierarchy.TryGetParent(type, out var parentType))
+                    BuildChain(parentType, activeStates, index - 1);
+
+                _chainBuffer.Add(CreateStateInternal(type));
                 return;
             }
 
@@ -58,6 +75,10 @@ namespace Aspid.Core.HSM
     public abstract class StateFactory<TState> : StateFactory
         where TState : IState
     {
+        protected StateFactory() { }
+
+        protected StateFactory(IStateHierarchy hierarchy) : base(hierarchy) { }
+
         protected sealed override void OnInitializeState(IState state) =>
             OnInitializeState((TState)state);
 
